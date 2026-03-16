1941a21631b28f7cb8d24385ffffd0b85043161a
maneshaYasinth <maneeshayasinthgunarathna@gmail.com>
2026-03-16 19:41:20 +0530

    added ecr tfs

---
+++ infra/modules/ecr/variables.tf
@@ -0,0 +1,14 @@
+variable "repository_name" {
+  type = string
+  default = "snip-infra"
+}
+
+variable "project_name" {
+  type = string
+  default = "snip-infra"
+}
+
+variable "environment"{
+  type = string
+  default = "dev"
+}
