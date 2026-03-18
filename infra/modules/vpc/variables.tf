7b043a879249227dcc59cc14468ac79115edc073
maneshaYasinth <maneeshayasinthgunarathna@gmail.com>
2026-03-19 00:19:11 +0530

    added vpc(only) module

---
+++ infra/modules/vpc/variables.tf
@@ -0,0 +1,14 @@
+variable "vpc_cidr" {
+  type = string
+  default = "10.0.0.0./16"
+}
+
+variable "project" {
+  type = string
+  default = "snip-infra"
+}
+
+variable "environment" {
+  type = string
+  default = "dev"
+}
